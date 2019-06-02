6f95c6ff6454fe80af9439bad0aa0290fb9a00df
Andrei V <andrei@ptaxa.net>
2019-06-02 20:18:15 +0300

    refact

--- fluedit/__main__.py
+++ fluedit/__main__.py
@@ -10,38 +10,41 @@ from .license import LicenseDialog
 from .main import MainWindow
 
 
-def except_hook(cls, exception, tb):
-    sys.__excepthook__(cls, exception, traceback)
-    fmt = traceback.format_exception(cls, exception, tb)
-    fmt = "\n".join(fmt)
-    b = QMessageBox.critical(None, "unexpected exception", fmt,
-                             QMessageBox.Abort | QMessageBox.Ignore,
-                             QMessageBox.Abort)
-    if b == QMessageBox.Abort:
-        qFatal(fmt)
+class Starter:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.app.setOrganizationName("ptaxa.net")
+        self.app.setApplicationName("FluEdit")
+        sys.excepthook = Starter.except_hook
 
+        settings = QSettings()
+        if settings.value("license_accepted") != "true":
+            self.lic_wnd = LicenseDialog()
+            self.lic_wnd.accepted.connect(self.on_license_accepted)
+            self.lic_wnd.show()
+        else:
+            self.main = MainWindow()
+            self.main.show()
 
-def _license_accepted():
-    MainWindow().show()
-    QSettings().setValue("license_accepted", "true")
+    def exec(self):
+        self.app.exec_()
 
+    def on_license_accepted(self):
+        QSettings().setValue("license_accepted", "true")
+        self.main = MainWindow()
+        self.main.show()
 
-def main():
-    app = QApplication(sys.argv)
-    sys.excepthook = except_hook
-    app.setOrganizationName("ptaxa.net")
-    app.setApplicationName("FluEdit")
-
-    settings = QSettings()
-    if settings.value("license_accepted") != "true":
-        window = LicenseDialog()
-        window.accepted.connect(_license_accepted)
-        window.show()
-    else:
-        wnd = MainWindow()
-        wnd.show()
-    app.exec_()
+    @staticmethod
+    def except_hook(cls, exception, tb):
+        sys.__excepthook__(cls, exception, traceback)
+        fmt = traceback.format_exception(cls, exception, tb)
+        fmt = "\n".join(fmt)
+        b = QMessageBox.critical(None, "unexpected exception", fmt,
+                                 QMessageBox.Abort | QMessageBox.Ignore,
+                                 QMessageBox.Abort)
+        if b == QMessageBox.Abort:
+            qFatal(fmt)
 
 
 if __name__ == '__main__':
-    main()
+    Starter().exec()
